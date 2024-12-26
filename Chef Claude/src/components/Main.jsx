135410d6aaf7fd6f3155d16e95cc81b0abd8b560
javierjericm <javierjericm@gmail.com>
2024-12-26 17:13:12 +0800

    Added addIngredient function in main form

--- Chef Claude/src/components/Main.jsx
+++ Chef Claude/src/components/Main.jsx
@@ -1,14 +1,31 @@
+import React from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([]);
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function addIngredient(event) {
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    event.preventDefault();
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={addIngredient} className="add-ingredient-form">
         <input
           aria-label="Add ingredient"
           type="text"
           placeholder="e.g. Oregano"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
