617fdd4f4a66b7045dbcee0a2687e5cff6c666b8
javierjericm <javierjericm@gmail.com>
2025-01-01 23:43:34 +0800

    Added generateAllNewDice function

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import Die from "./components/Die";
 
 export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  const diceElements = dice.map((num) => {
+    return <Die value={num} />;
+  });
+
+  function generateAllNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.ceil(Math.random() * 6);
+      newDice.push(randomNumber);
+    }
+    return newDice;
+  }
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 }
