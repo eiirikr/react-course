5adaa27a9dab3239bab43fcfe856a9a8a342601b
javierjericm <javierjericm@gmail.com>
2025-01-01 22:08:48 +0800

    Added Die component and its styles

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,3 +1,20 @@
+import Die from "./components/Die";
+
 export default function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 }
