7ae2830eb4b8154165746a3262e4502c5dc487f1
javierjericm <javierjericm@gmail.com>
2025-01-03 23:34:44 +0800

    Refactored rollDice function

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -14,7 +14,11 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        !die.isHeld ? { ...die, value: Math.ceil(Math.random() * 6) } : die
+      )
+    );
   }
 
   function hold(id) {
