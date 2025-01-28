6bedb30c9d117d85185363e4229aa79e891de340
javierjericm <javierjericm@gmail.com>
2025-01-28 22:49:55 +0800

    Display missed letter when lost

--- Assembly Endgame/src/App.jsx
+++ Assembly Endgame/src/App.jsx
@@ -57,9 +57,13 @@ export default function AssemblyEndgame() {
   });
 
   const letterElements = currentWord.split("").map((letter, index) => {
+    const shouldRevealLetter = isGameLost || guessedLetters.includes(letter);
+    const letterClassName = clsx(
+      isGameLost && !guessedLetters.includes(letter) && "missed-letter"
+    );
     return (
-      <span key={index}>
-        {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+      <span key={index} className={letterClassName}>
+        {shouldRevealLetter ? letter.toUpperCase() : ""}
       </span>
     );
   });
